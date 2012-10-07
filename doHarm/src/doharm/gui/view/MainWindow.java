31d415d1e620ff1d7b2908c42f946e76fc9348c1
haydnnewport <haydnnewport@gmail.com>
2012-10-08 12:55:41 +1300

    Added HealthBar and proper message box

--- doHarm/src/doharm/gui/view/MainWindow.java
+++ doHarm/src/doharm/gui/view/MainWindow.java
@@ -7,13 +7,15 @@ import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JTextPane;
 
+import net.miginfocom.swing.MigLayout;
+import doharm.gui.extras.EjectorQueue;
 import doharm.gui.input.KeyboardManager;
 import doharm.gui.input.MenuButtonListener;
 import doharm.gui.input.MouseManager;
 import doharm.logic.Game;
 import doharm.rendering.WorldRenderer;
 
-public class MainWindow{
+public class MainWindow {
 	private static final long serialVersionUID = 1L;
 	private static final String TITLE = "OUR GAME";
 	public static final int MAXIMIZED = 1;
@@ -26,30 +28,27 @@ public class MainWindow{
 	private WorldCanvas canvas;
 	private MouseManager mouseManager;
 	private KeyboardManager keyboardManager;
+	private JTextPane textPane;
+	private EjectorQueue<String> messages;
 
 	public MainWindow(Game game) {
 		// TODO window close
 
 		WorldRenderer renderer = new WorldRenderer(game);
-		canvas = new WorldCanvas(game,renderer);
-		JTextPane tp = new JTextPane();
-		tp.setContentType("text/html");
-		tp.setOpaque(false);
-		tp.setText("<p style=\"color:red\">Words</p>");
-		tp.setFocusable(false);
-		canvas.setLayout(new BorderLayout());
-		canvas.add(tp, BorderLayout.EAST);
-		mouseManager = new MouseManager(game,renderer);
-
+		canvas = new WorldCanvas(game, renderer);
+		textPane = new JTextPane();
+		textPane.setContentType("text/html");
+		textPane.setOpaque(false);
+		textPane.setFocusable(false);
+		messages = new EjectorQueue<String>(10);
+		canvas.setLayout(new MigLayout("fill"));
+		canvas.add(textPane, "y container.h-200, x 70%, w 30%");
+		canvas.add(new HealthBar(), "y 5, x container.w/2-50");
+		mouseManager = new MouseManager(game, renderer);
 		keyboardManager = new KeyboardManager(this,game.getCamera());
-		
-		
 		this.game = game;
-
 		state = MAXIMIZED;
-		
 		toggleSize();
-		canvas.requestFocusInWindow();
 	}
 
 	public void toggleSize() {
@@ -65,22 +64,24 @@ public class MainWindow{
 		} else if (state == MAXIMIZED) {
 			frame.setUndecorated(false);
 			frame.setBounds(0, 0, 800, 600);
-			//frame.pack();
 			state = MINIMIZED;
 		}
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //TODO change to window listener and handle exit confirmation
-
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // TODO change to
+																// window
+																// listener and
+																// handle exit
+																// confirmation
 		addListeners();
 		frame.setVisible(true);
-		
+
 	}
-	
-	public void toggleMenu(){
-		if (menu == null){
-			//Make only one menu, so we don't end up with lots
+
+	public void toggleMenu() {
+		if (menu == null) {
+			// Make only one menu, so we don't end up with lots
 			menu = new MenuScreen(new MenuButtonListener(this));
 		}
-		if(menu.isShowing()){
+		if (menu.isShowing()) {
 			canvas.remove(menu);
 			canvas.requestFocusInWindow();
 			canvas.addKeyListener(keyboardManager);
@@ -98,14 +99,27 @@ public class MainWindow{
 		canvas.addMouseListener(mouseManager);
 		canvas.addMouseMotionListener(mouseManager);
 		canvas.addKeyListener(keyboardManager);
+		canvas.requestFocusInWindow();
 	}
 
-	public JFrame getFrame(){
+	public JFrame getFrame() {
 		return frame;
 	}
-	public void repaint(){
+
+	public void repaint() {
 		frame.repaint();
-		//System.out.println(frame.getFocusOwner());
+		// System.out.println(frame.getFocusOwner());
+	}
+
+	public void addMessage(String text) {
+		messages.offer(text);
+		StringBuilder sb = new StringBuilder();
+		sb.append("<div style=\"font-family:sans-serif; font-weight:bold\">");
+		for (String s : messages) {
+			sb.append(s);
+		}
+		sb.append("</div>");
+		textPane.setText(sb.toString());
 	}
 
 }
