211db381219f53693f0172237df2dbfad6d4bc31
bewickrola <bewickrola@myvuw.ac.nz>
2012-10-07 20:31:01 +1300

    Added sets of added / removed entities, used for networking.

--- doHarm/src/doharm/logic/entities/EntityFactory.java
+++ doHarm/src/doharm/logic/entities/EntityFactory.java
@@ -1,14 +1,23 @@
 package doharm.logic.entities;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import doharm.logic.world.World;
 
 public class EntityFactory extends AbstractEntityFactory<AbstractEntity>
 {
 	private IDManager idManager;
+	private Set<AbstractEntity> addedEntities;
+	private Set<AbstractEntity> removedEntities;
+	
 	public EntityFactory(World world, IDManager idManager) 
 	{
 		super(world, null);
 		this.idManager = idManager;
+		addedEntities = new HashSet<AbstractEntity>();
+		removedEntities = new HashSet<AbstractEntity>();
 	}
 	
 	@Override
@@ -16,10 +25,31 @@ public class EntityFactory extends AbstractEntityFactory<AbstractEntity>
 	{
 		super.removeEntity(entity);
 		idManager.freeID(entity.getID());
+		removedEntities.add(entity);
 	}
 	@Override
 	public void addEntity(AbstractEntity entity, int id)
 	{
 		entity.setID(id);
+		addedEntities.add(entity);
 	}
+	
+	public Set<AbstractEntity> getAddedEntities()
+	{
+		return Collections.unmodifiableSet(addedEntities);
+	}
+	
+	public Set<AbstractEntity> getRemovedEntities()
+	{
+		return Collections.unmodifiableSet(removedEntities);
+	}
+	public void clearAddedEntities()
+	{
+		addedEntities.clear();
+	}
+	public void clearRemovedEntities()
+	{
+		removedEntities.clear();
+	}
+	
 }
