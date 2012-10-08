7e3039158e18564fe218efcb305c1d5389170ebf
mclareadam <mclareadam@fidels.ecs.vuw.ac.nz>
2012-10-09 12:46:47 +1300

    Added JUnit 4 Library. Added an empty test class for network testing.

---
+++ doHarm/src/doharm/net/testing/EntityInfoTests.java
@@ -0,0 +1,14 @@
+package doharm.net.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EntityInfoTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
